42c646c86d5dd07252ce92434d5ca5f61d68a00e
Aditya <adityamishra12310@gmail.com>
2026-05-24 22:03:00 +0530

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://romantic-phoenix-61.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
